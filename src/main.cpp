c1f5ddc1180a8d3690356c0e856cb2216935bc87
wjx-sky <1992888987@qq.com>
2025-07-29 09:02:52 +0800

    第六天学习提交

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,18 @@
 int main(){
 
 
-    int a=10;
-    std::cout<<"第一次测试"<<a<<std::endl;
-    return 0;
+    int var=10;
+    int * ptr=&var;//ptr指向var
+    int * * ptr_adress = &ptr;//指针的指针
+    std::cout<<"var的地址"<<&var<<std::endl;
+    std::cout<<"ptr储存的数据"<<ptr<<std::endl;
+    std::cout<<"ptr的地址"<<&ptr<<std::endl;
+    std::cout<<"ptr_address储存的数据"<<ptr_adress<<std::endl;
+    std::cout<<"ptr_add的地址："<<&ptr_adress<<std::endl;
+    //访问对象
+    *ptr=43;//赋值以及修改i数据内容
+    std::cout<<*ptr<<std::endl;//由*得到指针指向对象var的值
 
+    return 0;
+    
 }
